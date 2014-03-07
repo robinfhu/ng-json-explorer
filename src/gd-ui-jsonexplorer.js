db11c5804f6865f1f4ef97257a19b99adaff42e3
Robin Hu <rhu@novus.com>
2014-03-07 14:48:09 -0500

    Added defensive code to skip processing if the JSON value is a blank string.

--- src/gd-ui-jsonexplorer.js
+++ src/gd-ui-jsonexplorer.js
@@ -4,16 +4,16 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 -Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
--Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the 
+-Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
--Neither the name of the Goldark nor the names of its contributors may be used to endorse or promote products derived from this software 
+-Neither the name of the Goldark nor the names of its contributors may be used to endorse or promote products derived from this software
 without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
@@ -31,6 +31,8 @@ angular.module('gd.ui.jsonexplorer', [])
 		},
 		link: function (scope, elem, attrs) {
 			attrs.$observe('jsonData', function (val) {
+				if (val === '') return;
+
 				var output = '';
 				var formatter = {};
 				formatter.jsString = function (s) {
@@ -46,7 +48,7 @@ angular.module('gd.ui.jsonexplorer', [])
         					delete has[ws];
       					}
     				}
-    				
+
     				s = JSON.stringify({a:s});
     				s = s.slice(6, -2);
     				for (ws in has) {
@@ -70,7 +72,7 @@ angular.module('gd.ui.jsonexplorer', [])
 					var hasContents = false;
     				var output = '';
     				var numProps = 0;
-    
+
     				for (var prop in json ) {
       					numProps++;
     				}
@@ -84,7 +86,7 @@ angular.module('gd.ui.jsonexplorer', [])
       					output += '</li>';
       					numProps--;
     				}
-    
+
     				if (hasContents) {
       					output = '[<ul class="array collapsible">' + output + '</ul>]';
     				} else {
@@ -103,7 +105,7 @@ angular.module('gd.ui.jsonexplorer', [])
 
     				for (var prop in json) {
       					hasContents = true;
-      					output += '<li>' + 
+      					output += '<li>' +
       					'<span class="prop"><span class="q">"</span>' + this.jsString(prop) +
                 			'<span class="q">"</span></span>: ' + this.valueToHtml(json[prop]);
 	      				if (numProps > 1) {
@@ -112,16 +114,16 @@ angular.module('gd.ui.jsonexplorer', [])
 	      				output += '</li>';
 	      				numProps--;
     				}
-    
+
 	    			if (hasContents) {
 	      				output = '{<ul class="obj collapsible">' + output + '</ul>}';
 	    			} else {
 	      				output = '{ }';
 	    			}
-	    
+
 	    			return output;
 				};
-				
+
 				formatter.valueToHtml = function (value) {
 					var type = value && value.constructor;
 					var output = '';
@@ -144,7 +146,7 @@ angular.module('gd.ui.jsonexplorer', [])
 
 					if (type == String) {
 						if (/^(http|https|file):\/\/[^\s]+$/i.test(value)) {
-        					output += '<a href="' + value + '"><span class="q">"</span>' + 
+        					output += '<a href="' + value + '"><span class="q">"</span>' +
         						this.jsString(value) + '<span class="q">"</span></a>';
       					} else {
         					output += '<span class="string">"' + this.jsString(value) + '"</span>';
@@ -160,14 +162,14 @@ angular.module('gd.ui.jsonexplorer', [])
 				formatter.jsonToHtml = function (json) {
 					return '<div class="gd-ui-json-explorer">' + this.valueToHtml(json) + '</div>';
 				};
-				
+
 				var json = JSON.parse(val);
 				var x = formatter.jsonToHtml(json);
 				elem.html(x);
 				function collapse (evt) {
 					var collapser = evt.target;
     				var target = collapser.parentNode.getElementsByClassName('collapsible');
-    
+
     				if (!target.length) {
       					return;
     				}
@@ -188,7 +190,7 @@ angular.module('gd.ui.jsonexplorer', [])
 				      collapser.innerHTML = '+';
     				}
 				}
-				
+
 				var collections = angular.element(elem)[0].getElementsByTagName('ul');
 				for (var i = 0; i < collections.length; i++) {
 					var collectionItem = collections[i];
@@ -199,7 +201,7 @@ angular.module('gd.ui.jsonexplorer', [])
 							collapser.innerHTML = '-';
 							collapser.addEventListener('click', collapse, false);
 							collectionItem.parentNode.insertBefore(collapser, collectionItem.parentNode.firstChild);
-						}						
+						}
 					}
 				}
 			});
